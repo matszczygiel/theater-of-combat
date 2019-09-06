1f7a8b7b83db44fe613cd2b6d75278b177829266
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-09-06 22:58:52 +0200

    started to implement ECS for units

--- source/unit/unit.cpp
+++ source/unit/unit.cpp
@@ -3,13 +3,10 @@
 #include "unit_components.h"
 #include "core/log.h"
 
-Unit::Unit(entityx::EntityManager &es, UnitType unit_type, std::string unit_name)
+Unit::Unit(UnitType unit_type, std::string unit_name)
     : type{unit_type}, name{unit_name} {
-    _entity = es.create();
     create_from_type(type);
 }
-
-Unit::~Unit() { _entity.destroy(); }
 
 void Unit::create_from_type(UnitType type) {
     switch (type)
--- source/unit/unit.h
+++ source/unit/unit.h
@@ -1,27 +1,86 @@
 #ifndef UNIT_H
 #define UNIT_H
 
+#include <map>
+#include <memory>
 #include <string>
+#include <typeinfo>
+#include <vector>
 
-#include <entityx/Entity.h>
+#include "core/log.h"
 
 enum class UnitType { heavy, mechanized };
 
 class Unit {
    public:
-    Unit(entityx::EntityManager &es, UnitType type, std::string name);
-    ~Unit();
+    using IdType = int;
 
-    Unit(const Unit &) = delete;
-    Unit &operator=(const Unit &) = delete;
-
-    const std::string name{};
-    const UnitType type{};
+    Unit(const Unit&) = delete;
+    Unit& operator=(const Unit&) = delete;
 
    private:
-    void create_from_type(UnitType type);
+    Unit() = default;
 
-    entityx::Entity _entity{};
+    void create_from_type(UnitType type);
+    std::string name{};
+    UnitType type{};
 };
+
+struct ComponentVecBase {};
+
+template <class Component>
+struct ComponentVec : public ComponentVecBase {
+    std::vector<Component> vec;
+};
+
+class ComponentPoll {
+   public:
+    template <class Component>
+    std::vector<Component>& get_container();
+
+   private:
+    std::map<std::type_info, std::unique_ptr<ComponentVecBase>> _components{};
+};
+
+template <class Component>
+std::vector<Component>& ComponentPoll::get_container() {
+    auto& base_ptr = _components[typeid(Component)];
+    if (base_ptr == nullptr) {
+        base_ptr = std::make_unique<ComponentVec<Component>>();
+    }
+    ComponentVec<Component>* ptr = base_ptr.get();
+    return ptr->vec;
+}
+
+struct ComponentBase {
+   private:
+    friend class UnitManager;
+    Unit::IdType owner{};
+};
+
+class UnitManager {
+   public:
+    using IdType = int;
+
+    IdType create(UnitType type, std::string name);
+
+    template <class Component, class... Args>
+    Component& assign_component(IdType id, Args&&... args);
+
+   private:
+    ComponentPoll _components{};
+
+    IdType _current_free_id{0};
+    std::map<IdType, Unit> _units{};
+};
+
+template <class Component, class... Args>
+Component& UnitManager::assign_component(IdType id, Args&&... args) {
+    app_assert(_units.count(id) == 1, "Unit with id: {} does not exists.", id);
+    auto& vec = _components.get_container<Component>();
+    auto& com = vec.emplace_back(std::forward<Args>(args)...);
+    com.owner = id;
+    return com;
+}
 
 #endif

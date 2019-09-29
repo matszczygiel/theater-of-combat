09fdc668e9c369d5237cbb05a89100e97517785a
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-09-29 10:08:35 +0200

    FightComponent

--- source/unit/unit_components.cpp
+++ source/unit/unit_components.cpp
@@ -3,9 +3,7 @@
 #include "core/log.h"
 
 MovementComponent::MovementComponent(int moving_points) noexcept
-    : moving_pts{moving_points},
-      position{},
-      _total_moving_pts{moving_points} {
+    : moving_pts{moving_points}, position{}, _total_moving_pts{moving_points} {
     app_assert(
         moving_points >= 0,
         "MovementComponent should be initialized with positive number of moving pts.");
@@ -14,3 +12,6 @@ MovementComponent::MovementComponent(int moving_points) noexcept
 const int& MovementComponent::total_moving_pts() const {
     return _total_moving_pts;
 }
+
+FightComponent::FightComponent(int strength_points) noexcept
+    : strength_pts{strength_points} {}
--- source/unit/unit_components.h
+++ source/unit/unit_components.h
@@ -29,8 +29,25 @@ struct MovementComponent : public ComponentBase {
 
 template <class Archive>
 void MovementComponent::serialize(Archive& archive) {
-    archive(cereal::base_class<ComponentBase>(this), CEREAL_NVP(_total_moving_pts),
-            CEREAL_NVP(moving_pts), CEREAL_NVP(position), CEREAL_NVP(immobilized));
+    archive(cereal::base_class<ComponentBase>(this),
+            CEREAL_NVP(_total_moving_pts), CEREAL_NVP(moving_pts),
+            CEREAL_NVP(position), CEREAL_NVP(immobilized));
+}
+
+struct FightComponent : public ComponentBase {
+    FightComponent() = default;
+    explicit FightComponent(int strength_points) noexcept;
+
+    int strength_pts{};
+    bool in_fight{false};
+
+    template <class Archive>
+    void serialize(Archive& archive);
+};
+
+template <class Archive>
+void FightComponent::serialize(Archive& archive) {
+    archive(CEREAL_NVP(strength_pts), CEREAL_NVP(in_fight));
 }
 
 #endif

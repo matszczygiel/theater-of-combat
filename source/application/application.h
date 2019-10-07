477e23231464dfe4f4d1a16b607aabc74d93c6e2
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-10-07 20:40:53 +0200

    snap

--- source/application/application.h
+++ source/application/application.h
@@ -17,17 +17,15 @@ class Application {
     virtual void clear_loop()                         = 0;
     virtual void finalize()                           = 0;
 
-    virtual void key_pressed_event(const sf::Keyboard::Key& key)           = 0;
-    virtual void key_released_event(const sf::Keyboard::Key& key)          = 0;
+    virtual void key_pressed_event(const sf::Keyboard::Key& key)                    = 0;
+    virtual void key_released_event(const sf::Keyboard::Key& key)                   = 0;
     virtual void mouse_button_pressed_event(const sf::Mouse::Button& button,
-                                            const sf::Vector2f& position)  = 0;
+                                            const sf::Vector2f& position)           = 0;
     virtual void mouse_button_released_event(const sf::Mouse::Button& button,
-                                             const sf::Vector2f& position) = 0;
-    virtual void mouse_wheel_scrolled_event(const float& delta)            = 0;
-    virtual void window_resize_event(const unsigned& width,
-                                     const unsigned& height)               = 0;
-    virtual void mouse_moved_event(const sf::Vector2f& position)           = 0;
-
+                                             const sf::Vector2f& position)          = 0;
+    virtual void mouse_wheel_scrolled_event(const float& delta)                     = 0;
+    virtual void window_resize_event(const unsigned& width, const unsigned& height) = 0;
+    virtual void mouse_moved_event(const sf::Vector2f& position)                    = 0;
 
     sf::RenderWindow _window{};
 

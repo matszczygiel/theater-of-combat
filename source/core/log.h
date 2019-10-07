320d3d7200a48186a8e6bd9cf3f79a927b347f3f
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-10-07 20:19:51 +0200

    cleaning dirs

--- source/core/log.h
+++ source/core/log.h
@@ -9,69 +9,69 @@
 #include <spdlog/sinks/dist_sink.h>
 
 namespace logger {
-std::shared_ptr<spdlog::sinks::dist_sink_mt>& get_distributing_sink();
-std::unique_ptr<spdlog::logger>& get_app_logger();
-std::unique_ptr<spdlog::logger>& get_engine_logger();
+std::shared_ptr<spdlog::sinks::dist_sink_mt>& get_distributing_sink() noexcept;
+spdlog::logger& get_app_logger() noexcept;
+spdlog::logger& get_engine_logger() noexcept;
 }  // namespace logger
 
 template <typename... Args>
 inline void engine_trace(const char* fmt, const Args&... args) {
-    logger::get_engine_logger()->trace(fmt, args...);
+    logger::get_engine_logger().trace(fmt, args...);
 }
 
 template <typename... Args>
 inline void engine_debug(const char* fmt, const Args&... args) {
-    logger::get_engine_logger()->debug(fmt, args...);
+    logger::get_engine_logger().debug(fmt, args...);
 }
 
 template <typename... Args>
 inline void engine_info(const char* fmt, const Args&... args) {
-    logger::get_engine_logger()->info(fmt, args...);
+    logger::get_engine_logger().info(fmt, args...);
 }
 
 template <typename... Args>
 inline void engine_warn(const char* fmt, const Args&... args) {
-    logger::get_engine_logger()->warn(fmt, args...);
+    logger::get_engine_logger().warn(fmt, args...);
 }
 
 template <typename... Args>
 inline void engine_error(const char* fmt, const Args&... args) {
-    logger::get_engine_logger()->error(fmt, args...);
+    logger::get_engine_logger().error(fmt, args...);
 }
 
 template <typename... Args>
 inline void engine_critical(const char* fmt, const Args&... args) {
-    logger::get_engine_logger()->critical(fmt, args...);
+    logger::get_engine_logger().critical(fmt, args...);
 }
 
 template <typename... Args>
 inline void app_trace(const char* fmt, const Args&... args) {
-    logger::get_app_logger()->trace(fmt, args...);
+    logger::get_app_logger().trace(fmt, args...);
 }
 
 template <typename... Args>
 inline void app_debug(const char* fmt, const Args&... args) {
-    logger::get_app_logger()->debug(fmt, args...);
+    logger::get_app_logger().debug(fmt, args...);
 }
 
 template <typename... Args>
 inline void app_info(const char* fmt, const Args&... args) {
-    logger::get_app_logger()->info(fmt, args...);
+    logger::get_app_logger().info(fmt, args...);
 }
 
 template <typename... Args>
 inline void app_warn(const char* fmt, const Args&... args) {
-    logger::get_app_logger()->warn(fmt, args...);
+    logger::get_app_logger().warn(fmt, args...);
 }
 
 template <typename... Args>
 inline void app_error(const char* fmt, const Args&... args) {
-    logger::get_app_logger()->error(fmt, args...);
+    logger::get_app_logger().error(fmt, args...);
 }
 
 template <typename... Args>
 inline void app_critical(const char* fmt, const Args&... args) {
-    logger::get_app_logger()->critical(fmt, args...);
+    logger::get_app_logger().critical(fmt, args...);
 }
 
 template <typename... Args>

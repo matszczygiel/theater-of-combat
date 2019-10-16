a81ed1ba0fee57045396f5883f2a5f5d4c2e88d5
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-10-16 21:01:46 +0200

    simplify network prompt

--- game/source/gui/network_prompt.cpp
+++ game/source/gui/network_prompt.cpp
@@ -12,47 +12,40 @@ void show_network_prompt(std::variant<Server, Client>& net, std::string title,
     }
 
     static int current_item = 0;
-    static int port         = 0;
     static std::string ip{};
     static const sf::IpAddress ip_local = sf::IpAddress::getLocalAddress();
     static sf::IpAddress ip_remote{};
-    static unsigned short bound_port = 0;
-    static bool server_accepting     = false;
+    static int bound_port        = 0;
+    static int port              = 0;
+    static bool server_accepting = false;
     ImGui::Combo("Connection type", &current_item, "Server\0Client\0");
     if (current_item == 0) {
-        ImGui::InputInt("Port", &port);
-        if (ImGui::Button("Bind to port")) {
-            auto& server = net.emplace<Server>();
-            if (server.listen_at_port(port)) {
-                bound_port       = server.get_port();
-                server_accepting = true;
-            } else {
-                server_accepting = false;
-            }
+        if (!std::holds_alternative<Server>(net)) {
+            net.emplace<Server>();
         }
 
+        auto& server = std::get<Server>(net);
+
         if (server_accepting) {
-            if (net.index() == 0) {
-                auto& server = std::get<0>(net);
-                if (server.accept_client()) {
-                    ip_remote        = server.get_remote_ip();
-                    server_accepting = false;
-                }
+            if (server.accept_client()) {
+                ip_remote        = server.get_remote_ip();
+                server_accepting = false;
             }
             if (ImGui::Button("Stop listening for clients")) {
                 server_accepting = false;
             }
         } else {
             if (ImGui::Button("Listen for clients")) {
-                if (net.index() == 0) {
-                    server_accepting = true;
-                }
+                server.listen_at_port(sf::Socket::AnyPort);
+                bound_port       = server.get_port();
+                server_accepting = true;
             }
         }
     }
     if (current_item == 1) {
         ImGui::InputInt("Port", &port);
         ImGui::InputText("Ip", &ip);
+        server_accepting = false;
         if (ImGui::Button("Connect")) {
             auto& client = net.emplace<Client>();
             if (client.connect_to_server(sf::IpAddress{ip}, port)) {

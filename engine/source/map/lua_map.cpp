db1d9755c2a09ed64a99bfbdb7fd48c4a2069a45
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-10-20 12:54:51 +0200

    snap

--- engine/source/map/lua_map.cpp
+++ engine/source/map/lua_map.cpp
@@ -11,25 +11,15 @@
 #include "map/types.h"
 
 namespace map {
-void lua_push_functions(sol::state& lua) {
+
+static void push_coordinates(sol::state& lua) {
     auto coord = lua.new_usertype<HexCoordinate>(
         "Coord", sol::constructors<HexCoordinate(), HexCoordinate(int, int)>());
     coord["q"] = &HexCoordinate::q;
     coord["p"] = &HexCoordinate::p;
+}
 
-    lua.new_enum("HexType", "field", HexType::field, "forest", HexType::forest);
-
-    auto hex = lua.new_usertype<HexSite>(
-        "HexSite", sol::constructors<HexSite(HexCoordinate, HexType)>());
-    hex["type"] = &HexSite::type;
-
-    lua.new_enum("RiverType", "small", RiverType::small, "stream", RiverType::stream);
-
-    auto river = lua.new_usertype<RiverSite>(
-        "RiverSite",
-        sol::constructors<RiverSite(HexCoordinate, HexCoordinate, RiverType)>());
-    river["type"] = &RiverSite::type;
-
+static void push_map(sol::state& lua) {
     auto map = lua.new_usertype<Map>("Map", sol::constructors<Map(), Map(const Map&)>());
     map["test_map"]                    = &Map::create_test_map;
     map["insert_hex"]                  = sol::resolve<Map&(HexSite)>(&Map::insert);
@@ -42,5 +32,27 @@ void lua_push_functions(sol::state& lua) {
         }
         return ss.str();
     };
+}
+
+static void push_hex_type(sol::state& lua) {
+    lua.new_enum("HexType", "field", HexType::field, "forest", HexType::forest);
+    auto hex = lua.new_usertype<HexSite>(
+        "HexSite", sol::constructors<HexSite(HexCoordinate, HexType)>());
+    hex["type"] = &HexSite::type;
+}
+
+static void push_river_type(sol::state& lua) {
+    lua.new_enum("RiverType", "small", RiverType::small, "stream", RiverType::stream);
+    auto river = lua.new_usertype<RiverSite>(
+        "RiverSite",
+        sol::constructors<RiverSite(HexCoordinate, HexCoordinate, RiverType)>());
+    river["type"] = &RiverSite::type;
+}
+
+void lua_push_functions(sol::state& lua) {
+    push_coordinates(lua);
+    push_hex_type(lua);
+    push_river_type(lua);
+    push_map(lua);
 }
 }  // namespace map
